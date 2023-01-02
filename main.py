6f3966663aff2e786f4aba80c9620f0cde111927
Alex <alexx.j.jackson@gmail.com>
2023-01-02 15:12:59 -0800

    work on getting message send to work properly

--- main.py
+++ main.py
@@ -5,10 +5,17 @@ intents.messages = True
 
 client = discord.Client(intents=intents)
 
+
 @client.event
-
 async def on_message(message):
-    if message.content == '!move':
-      await message.channel.send('Moving to the next channel')
+    print("Message received: ", message.content)
+    print("Channel: ", message.channel)
+    if message.content.startswith == "!move":
+        print("message sent")
+        try:
+            await message.channel.send("Moving out!")
+        except Exception as e:
+            print("Error: ", e)
 
-client.run('MTA1OTU4MzUxMzQ5OTc5NTQ2Ng.GJIR3D.bzGNxI0iOblfjg-xURAXa3-yh_wtj-LpuHKZSw')
+
+client.run("MTA1OTU4MzUxMzQ5OTc5NTQ2Ng.GJIR3D.bzGNxI0iOblfjg-xURAXa3-yh_wtj-LpuHKZSw")
