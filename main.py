3b24b717199f560b12f325ad6a7b59cefcbc547f
Alex <alexx.j.jackson@gmail.com>
2023-01-02 14:48:33 -0800

    add baseline for bot

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
 import discord
 
-client = discord.Client()
+intents = discord.Intents.default()
+intents.messages = True
 
+client = discord.Client(intents=intents)
+
+@client.event
+
+async def on_message(message):
+    if message.content == '!move':
+      await message.channel.send('Moving to the next channel')
+
+client.run('MTA1OTU4MzUxMzQ5OTc5NTQ2Ng.GJIR3D.bzGNxI0iOblfjg-xURAXa3-yh_wtj-LpuHKZSw')
